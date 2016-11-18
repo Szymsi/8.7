32c2717f4bc494f6c6d2855e48c3129b858242c3
Szymsi <sienkiewicz.szymon@gmail.com>
2016-11-18 11:08:19 +0100

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+var womensNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var mensNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = womensNames.concat(mensNames);
+console.log(allNames);
+var newName = 'Marian';
+if(allNames.indexOf(newName) == -1) {
+  allNames.push(newName)
+}
+console.log(allNames);
